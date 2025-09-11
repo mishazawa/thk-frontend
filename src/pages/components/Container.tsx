09db55eab04e817aefcc44025680729079faeab9
asd <macintoshkrug@gmail.com>
2025-09-11 23:34:58 +0300

    added css prop

--- src/pages/components/Container.tsx
+++ src/pages/components/Container.tsx
@@ -1,8 +1,19 @@
 import type { ReactNode } from "react";
 
-export function Page({ children }: { children: ReactNode }) {
+export function Page({
+  children,
+  className = "",
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
-    <div className="p-5 container-fluid flex-column d-flex flex-grow-1 align-items-center justify-content-around">
+    <div
+      className={
+        "p-5 container-fluid flex-column d-flex flex-grow-1 align-items-center justify-content-around " +
+        className
+      }
+    >
       {children}
     </div>
   );
